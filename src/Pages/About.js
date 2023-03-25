2e18b525859f9869d5a5e48328474a8d1df9c35f
Bhavesh <bhavesh.mittal2810@gmail.com>
2023-03-25 13:32:31 +0530

    New Achievements

--- src/Pages/About.js
+++ src/Pages/About.js
@@ -1,7 +1,7 @@
 import '../Stylesheets/About.css';
 import { LazyLoadImage } from 'react-lazy-load-image-component';
 import { BsGithub, BsLinkedin } from 'react-icons/bs';
-import { MdEmail } from 'react-icons/md';
+import { FaFileAlt } from 'react-icons/fa';
 import AboutCard from '../components/AboutCard';
 import myImg2 from '../assets/myImg2.jpg';
 
@@ -14,14 +14,13 @@ export default function About() {
                 </div>
                 <div className='aboutMeDataContainer'>
                     <div className='contactsContainer'>
-                        <a className='contactLink' href='https://github.com/Bhavesh-Mittal' rel='noopener noreferrer'><BsGithub className='contactIcon' /></a>
-                        <a className='contactLink' href='https://www.linkedin.com/in/bhavesh-mittal-602a36254/' rel='noopener noreferrer'><BsLinkedin className='contactIcon' /></a>
-                        <a className='contactLink' href='mailto:bhavesh.mittal2810@gmail.com' rel='noopener noreferrer'><MdEmail className='contactIcon' /></a>
+                        <a className='contactLink' href='https://github.com/Bhavesh-Mittal' target='_blank' rel='noopener noreferrer' title='GitHub'><BsGithub className='contactIcon' /></a>
+                        <a className='contactLink' href='https://www.linkedin.com/in/bhavesh-mittal-602a36254/' target='_blank' rel='noopener noreferrer' title='LinkedIn'><BsLinkedin className='contactIcon' /></a>
+                        <a className='contactLink' href='https://docs.google.com/document/d/1omPJKCqjom1tmPlV6-qbcP7mNk9cRsg9/edit?usp=sharing&ouid=111868942714712607093&rtpof=true&sd=true' target='_blank' rel='noopener noreferrer' title='Resume'><FaFileAlt className='contactIcon' /></a>
                     </div>
                     <AboutCard title='Personal Information'
                         point1={ <p>Full Name : Bhavesh Mittal</p> }
                         point2={ <p>Date Of Birth : 28/10/2006</p> }
-                        point3={ <p>Zodiac Sign : Scorpio</p> }
                     />
                     <AboutCard title='Education'
                         point1={ <p>Delhi Public School, New Delhi <br /> (2012-2024)</p> }
--- src/Pages/Achievements.js
+++ src/Pages/Achievements.js
@@ -1,10 +1,20 @@
 import '../Stylesheets/Achievements.css';
 import AchievementCard from '../components/AchievementCard';
 import blueBlazer from '../assets/blueBlazer.jpg';
+import internshipImg from '../assets/internshipImg.jpg';
+import mtaImg from '../assets/mtaImg.jpg';
 
 export default function Achievements() {
     return (
         <>
+            <div className='cardsContainer'>
+                <AchievementCard image={ internshipImg } title='Pine Labs Internship' description='I used Python to automate Excel and Outlook for a live RPA project at PineLabs. This taught me how to problem-solve in a structured way, map process improvements, and deliver successful solutions for resolving chargeback cycles' />
+                <AchievementCard image={ mtaImg } title='Microsoft Technology Associate Certificate' description='This helped me learn the universal programming concepts, Python syntax and semantics, standard language infrastructure and fundamental programming techniques' />
+            </div>
+            <div className='cardsContainer'>
+                <AchievementCard image='https://images.pexels.com/photos/3571094/pexels-photo-3571094.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' title='WhatsApp Clone' description={ <p>I built a simple <a href='https://github.com/Bhavesh-Mittal/Whatsapp' target='_blank' rel='noopener noreferrer'>copy of WhatsApp</a> using React Native : specifically the <a href='https://www.npmjs.com/package/react-native-gifted-chat?activeTab=readme' target='_blank' rel='noopener noreferrer'>Gifted Chat</a> package. This helped me understand the core fundamentals of React Native and how real-time messaging works</p> } />
+                <AchievementCard image='https://i.stack.imgur.com/RgQha.png?s=256' title='Hand Detection' description={ <p>I developed a <a href='https://github.com/Bhavesh-Mittal/Hand-Detection' target='_blank' rel='noopener noreferrer'>hand detection program</a> using Python computer vision techniques. Through this project, I learned how to use the OpenCV library to detect and track objects in real-time video stream</p> } />
+            </div>
             <div className='cardsContainer'>
                 <AchievementCard image='https://images.pexels.com/photos/1019470/abacus-mathematics-addition-subtraction-1019470.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' title='Abacus' description='Completed all 10 levels of Abacus in 8 years; in the process helping me build a strong academic foundation, boosting my creativity and increasing my concentration' />
                 <AchievementCard image='https://images.pexels.com/photos/5412499/pexels-photo-5412499.jpeg?auto=compress&cs=tinysrgb&w=1260&h=750&dpr=2' title='Vedic Maths' description='Completed all 3 levels of Vedic Maths in 3 years; in the process helping me solve tricky calculations in a few seconds and enhancing my logical reasoning skills' />
@@ -14,8 +24,8 @@ export default function Achievements() {
                 <AchievementCard image={ blueBlazer } title='School Blue Blazer' description='Awarded the school blue blazer twice (2019 & 2022) for scoring above 80% for three consecutive years' />
             </div>
             <div className='cardsContainer'>
-                <AchievementCard image='https://d3q0c4wmynnr7l.cloudfront.net/sof_logo-01_-_copy.jpg' title='SOF Olympiads' description={ <p>Participated in many olympiads hosted by <a href='https://www.sofworld.org' rel='noopener noreferrer'>Science Olympiad Foundation (SOF)</a> like IMO, IEO and NSO; winning gold and solver medals in IMO and NSO</p> } />
-                <AchievementCard image='https://www.ehfworld.com/ERP/logo3.png' title='EHF Olympiads' description={ <p>Took part in various olympiads held by <a href='https://www.eduhealfoundation.org/' rel='noopener noreferrer'>EduHeal Foundation (EHF)</a> including NIMO, IGKO and NSSO; winning a silver medal in NIMO</p> } />
+                <AchievementCard image='https://d3q0c4wmynnr7l.cloudfront.net/sof_logo-01_-_copy.jpg' title='SOF Olympiads' description={ <p>Participated in many olympiads hosted by <a href='https://www.sofworld.org' target='_blank' rel='noopener noreferrer'>Science Olympiad Foundation (SOF)</a> like IMO, IEO and NSO; winning gold and solver medals in IMO and NSO</p> } />
+                <AchievementCard image='https://www.ehfworld.com/ERP/logo3.png' title='EHF Olympiads' description={ <p>Took part in various olympiads held by <a href='https://www.eduhealfoundation.org/' target='_blank' rel='noopener noreferrer'>EduHeal Foundation (EHF)</a> including NIMO, IGKO and NSSO; winning a silver medal in NIMO</p> } />
             </div>
         </>
     );
